51d41cd79f51acad6bcca46bbe65672ef03f3004
Dogukandogann <dogukn.dogann@gmail.com>
2024-09-24 00:38:46 +0300

    Consuming example

--- Consumer/Consumer/Program.cs
+++ Consumer/Consumer/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri =new("amqps://aoldppsp:f6YNTlBi7kuoVqHotf1lloh6H9wgd-sx@toad.rmq.cloudamqp.com/aoldppsp");
+
+//Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Kuyruk Oluşturma
+//Consumerda'da kuyruk publisher'daki ile birebir aynı yapılandırma tanımlanmalıdır.
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Mesaj okuma
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue",true,consumer);
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlenidği yer
+    //e.Body : kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+    //e.Body.Span() veya e.Body.ToArray() mesajın byte verisini getirecektir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+Console.Read();
--- Publisher/Publisher/Program.cs
+++ Publisher/Publisher/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Bağlantı oluşturma
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://aoldppsp:f6YNTlBi7kuoVqHotf1lloh6H9wgd-sx@toad.rmq.cloudamqp.com/aoldppsp");
+
+//Bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Kuyruk oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Mesaj gönderme
+//RabbitMq kuyruğa atacağı mesajları byte olarak kabul etmektedir.
+byte[] message = Encoding.UTF8.GetBytes("Hello World!");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+Console.Read();
