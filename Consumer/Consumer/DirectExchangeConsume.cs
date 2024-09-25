f638a0ead31ee231a6538367a4d075f72f6e2a3f
Dogukandogann <dogukn.dogann@gmail.com>
2024-09-25 21:04:31 +0300

    DirectExchange example

---
+++ Consumer/Consumer/DirectExchangeConsume.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    public static class DirectExchangeConsume
+    {
+        public static void Publisher()
+        {
+            //Bağlantı oluşturma
+            ConnectionFactory factory = new();
+            factory.Uri = new("amqps://aoldppsp:f6YNTlBi7kuoVqHotf1lloh6H9wgd-sx@toad.rmq.cloudamqp.com/aoldppsp");
+
+            //Bağlantıyı aktifleştirme ve kanal açma
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            //Publisherda olan exchange ile birebir aynı isim ve type olmalıdır.
+            channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+            //Publisher tarafında routingKey'de bulunan değerdeki kuyruğa gönderilen mesajları kendi oluşturduğumuz kuyruğa yönlendirerek tüketmemiz gerekmektedir.
+            string queueName = channel.QueueDeclare().QueueName;
+
+            //routingKey ile kuyruğu bağlama işlemi.
+            channel.QueueBind(queue: queueName,exchange: "direct-exchange-example",routingKey: "direct-queue-example");
+            
+            EventingBasicConsumer consumer = new(channel);
+            channel.BasicConsume(queue: queueName, true, consumer);
+            consumer.Received += (sender, e) =>
+            {
+                string message = Encoding.UTF8.GetString(e.Body.Span);
+                Console.WriteLine(message);
+            };
+        }
+    }
+}
---
+++ Publisher/Publisher/DirectExchangePublish.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    public static class DirectExchangePublish
+    {
+        public static void Publisher()
+        {
+            //Bağlantı oluşturma
+            ConnectionFactory factory = new();
+            factory.Uri = new("amqps://aoldppsp:f6YNTlBi7kuoVqHotf1lloh6H9wgd-sx@toad.rmq.cloudamqp.com/aoldppsp");
+
+            //Bağlantıyı aktifleştirme ve kanal açma
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            //Exchange oluşturma
+            channel.ExchangeDeclare(exchange:"direct-exchange-example",type:ExchangeType.Direct);
+
+            //Kuyruk oluşturma
+            while (true)
+            {
+                Console.Write("Mesaj :");
+                string message = Console.ReadLine();
+                byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "direct-exchange-example",routingKey:"direct-queue-example",body:byteMessage);
+            }
+        }
+
+    }
+}
