d9342d465607ecc5c4b8a8483b320bb8ae605b6b
Dogukandogann <dogukn.dogann@gmail.com>
2024-09-26 14:42:59 +0300

    TopicExchange Example

---
+++ Consumer/Consumer/TopicExchangeConsume.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    public static class TopicExchangeConsume
+    {
+        public static async void Consume()
+        {
+            ConnectionFactory factory = new();
+            factory.Uri = new("amqps://aoldppsp:f6YNTlBi7kuoVqHotf1lloh6H9wgd-sx@toad.rmq.cloudamqp.com/aoldppsp");
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "topix-exchange-example", type: ExchangeType.Topic);
+
+            Console.WriteLine("Dinlenecek Topic?");
+            string topic = Console.ReadLine();
+
+            string queueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queue: queueName,exchange: "topix-exchange-example",routingKey:topic);
+
+            EventingBasicConsumer consumer = new(channel);
+            channel.BasicConsume(queue:queueName,autoAck:true,consumer:consumer);
+            consumer.Received +=(sender,e) =>
+            {
+                string message = Encoding.UTF8.GetString(e.Body.Span);
+                Console.WriteLine(message);
+            };
+        }
+    }
+}
---
+++ Publisher/Publisher/TopicExchangePublish.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    public static class TopicExchangePublish
+    {
+        public static async void Publish()
+        {
+            ConnectionFactory factory = new();
+            factory.Uri = new("amqps://aoldppsp:f6YNTlBi7kuoVqHotf1lloh6H9wgd-sx@toad.rmq.cloudamqp.com/aoldppsp");
+
+            //Bağlantıyı aktifleştirme ve kanal açma
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "topix-exchange-example", type: ExchangeType.Topic);
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(200);
+                byte[] message = Encoding.UTF8.GetBytes($"Hello World! {i}");
+                Console.WriteLine("Topic Belirtiniz");
+                string topic = Console.ReadLine();
+                channel.BasicPublish(exchange: "topix-exchange-example",routingKey: topic,body:message);
+            }
+        }
+    }
+}
