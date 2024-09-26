d7fb12bb6106aaa2f69792a1673682a5d080d077
Dogukandogann <dogukn.dogann@gmail.com>
2024-09-26 14:00:32 +0300

    Fanout Example

---
+++ Consumer/Consumer/FanoutConsume.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    public static class FanoutConsume
+    {
+        public static void Publisher()
+        {
+            //Bağlantı oluşturma
+            ConnectionFactory factory = new();
+            factory.Uri = new("amqps://aoldppsp:f6YNTlBi7kuoVqHotf1lloh6H9wgd-sx@toad.rmq.cloudamqp.com/aoldppsp");
+
+            //Bağlantıyı aktifleştirme ve kanal açma
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
+
+            Console.WriteLine("Kuyruk Adı Giriniz");
+            string _queueName = Console.ReadLine();
+            channel.QueueDeclare(queue:_queueName,exclusive:false);
+
+            channel.QueueBind(queue:_queueName,exchange: "fanout-exchange-example",routingKey:string.Empty);
+
+            EventingBasicConsumer consumer = new(channel);
+            channel.BasicConsume(queue: _queueName, true, consumer);
+
+            consumer.Received += (sender, e) =>
+            {
+                string message = Encoding.UTF8.GetString(e.Body.Span);
+                Console.WriteLine(message);
+            }; 
+        }
+    }
+}
---
+++ Publisher/Publisher/FanoutPublish.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    public static class FanoutPublish
+    {
+        public static async void Publis()
+        {
+            //Bağlantı oluşturma
+            ConnectionFactory factory = new();
+            factory.Uri = new("amqps://aoldppsp:f6YNTlBi7kuoVqHotf1lloh6H9wgd-sx@toad.rmq.cloudamqp.com/aoldppsp");
+
+            //Bağlantıyı aktifleştirme ve kanal açma
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange:"fanout-exchange-example",type:ExchangeType.Fanout);
+
+            for (int i = 0; i < 100; i++)
+            {
+                await Task.Delay(200);
+                byte[] message = Encoding.UTF8.GetBytes($"Hello RabbitMQ : {i}");
+                channel.BasicPublish(exchange: "fanout-exchange-example",routingKey:string.Empty,body:message);
+            }
+        } 
+    }
+}
